2f5d29d40497fcefb4dc24541307fa82521f56e4
Harikrishnan M B <harikrishnan.mb.dev@gmail.com>
2026-01-04 20:18:15 +0530

    04-jan-latest-1.1

--- AdminApp/urls.py
+++ AdminApp/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from AdminApp import views
 
 urlpatterns = [
-    path('dashboard/',views.dashboard,name="dashboard")
+    path('dashboard/',views.dashboard,name="dashboard"),
+    path('add_service_category_form_page/',views.add_service_category_form_page,name="add_service_category_form_page"),
+    path('display_service_categories_page/',views.display_service_categories_page,name="display_service_categories_page"),
+    path('save_service_category/',views.save_service_category,name="save_service_category"),
+    path('edit_service_category_page/<int:category_id>/',views.edit_service_category_page,name="edit_service_category_page"),
+    path('update_service_category/<int:category_id>/',views.update_service_category,name="update_service_category"),
+    path('delete_service_category/<int:category_id>/',views.delete_service_category,name="delete_service_category"),
 ]
--- AdminApp/views.py
+++ AdminApp/views.py
@@ -1,8 +1,61 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from django.utils.datastructures import MultiValueDictKeyError
 from django.core.files.storage import FileSystemStorage
+from AdminApp.models import *
 
 # Create your views here.
 
 def dashboard(request):
     return render(request,"dashboard.html")
+
+# Category Views
+
+def add_service_category_form_page(request):
+    return render(request,"add-service-category-page.html")
+
+def save_service_category(request):
+
+    if request.method == 'POST':
+        category_name = request.POST.get("category_name")
+        category_description = request.POST.get("category_description")
+        is_active = bool(request.POST.get('is_active'))
+
+        ServiceCategoryDb(
+            category_name = category_name,
+            category_description = category_description,
+            is_active = is_active
+
+        ).save()
+
+        return redirect(add_service_category_form_page)
+
+
+def display_service_categories_page(request):
+    service_categories = ServiceCategoryDb.objects.all()
+    return render(request,"display-service-categories-page.html",{"service_categories":service_categories})
+
+def edit_service_category_page(request,category_id):
+    category = ServiceCategoryDb.objects.get(id=category_id)
+    return render(request,"edit-service-category-page.html",{"category":category})
+
+def update_service_category(request,category_id):
+
+    if request.method == 'POST':
+        category_name = request.POST.get("category_name")
+        category_description = request.POST.get("category_description")
+        is_active = bool(request.POST.get('is_active'))
+
+        ServiceCategoryDb.objects.filter(id=category_id).update(
+            category_name = category_name,
+            category_description = category_description,
+            is_active = is_active
+
+        )
+
+        return redirect(display_service_categories_page)
+
+
+def delete_service_category(request,category_id):
+    ServiceCategoryDb.objects.filter(id=category_id).delete()
+    return redirect(display_service_categories_page)
+
