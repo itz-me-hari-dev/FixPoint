79691107bc66c187392400f15dc39ce95e7dffa6
Harikrishnan M B <harikrishnan.mb.dev@gmail.com>
2026-01-10 23:24:30 +0530

    10-jan-latest-1.2

--- AdminApp/urls.py
+++ AdminApp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from AdminApp import views
 
 urlpatterns = [
-    path('dashboard/',views.dashboard,name="dashboard"),
+    path('',views.dashboard,name="dashboard"),
     path('add_service_category_form_page/',views.add_service_category_form_page,name="add_service_category_form_page"),
     path('display_service_categories_page/',views.display_service_categories_page,name="display_service_categories_page"),
     path('save_service_category/',views.save_service_category,name="save_service_category"),
