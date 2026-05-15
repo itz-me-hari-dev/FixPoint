bad100e8523e95d7c7e104f0b71a867a6758d198
Harikrishnan M B <harikrishnan.mb.dev@gmail.com>
2026-05-15 22:46:40 +0530

    15-may-1.2

--- AdminApp/models.py
+++ AdminApp/models.py
@@ -28,7 +28,6 @@ class CustomerContactDb(models.Model):
     def __str__(self):
         return f"{self.full_name} - {self.email}"
 
-
 class ServiceProviderContactDb(models.Model):
 
     user = models.ForeignKey(
